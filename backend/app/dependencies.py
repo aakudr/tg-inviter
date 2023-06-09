19ccd2d0e675ea16384406fa6cf904eb6ac66b9f
eternity <aakudr2k3@gmail.com>
2023-06-10 02:52:58 +0600

    backend structure

---
+++ backend/app/dependencies.py
@@ -0,0 +1,12 @@
+"""
+List of dependencies for the FastAPI app
+
+- get_db: get the database connection
+- get_config: get the config object
+- get_proxy: get the proxy object
+- get_action: get the action object
+- get_logs: get the logs object
+- get_status: get the status object
+
+- run_action_middleware: run an action
+"""
